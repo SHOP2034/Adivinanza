9304d34b301275afe7dd7f8ba5fc62b6d7a2bbe2
SHOP2034 <SHOPPAGINAS@GMAIL.COM>
2025-12-24 03:54:44 -0300

    Add files via upload

---
+++ categorias/peliculas_series.js
@@ -0,0 +1,12 @@
+
+registrarCategoria('peliculas_series', {
+    nombre: '🎬 Películas y Series',
+    palabras: [
+        "Titanic", "Avatar", "Harry Potter", "Star Wars", "Frozen",
+        "El Padrino", "Toy Story", "Matrix", "Shrek", "Avengers",
+        "Joker", "Barbie", "Coco", "El Rey León", "Spiderman",
+        "Stranger Things", "Game of Thrones", "Friends", "Breaking Bad", 
+        "El Juego del Calamar", "La Casa de Papel", "Merlina", "Batman"
+    ]
+});
+
